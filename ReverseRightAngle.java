d8709401774ca21cbed99e45602d2f5ae66aec35
pavani polipalli <polipallipavani54@gmail.com>
2025-10-30 13:30:52 +0530

    ReverseRightAngle

---
+++ ReverseRightAngle.java
@@ -0,0 +1,26 @@
+/*Print the below pattern
+ * * * *
+   * * *
+     * *
+       **/
+
+package com.codegnan.patterns;
+
+public class ReverseRightAngle {
+
+	public static void main(String[] args) {
+		int rows=4 ;
+		for(int i=0;i<rows;i++) {
+			for(int j=0;j<i;j++) {
+			System.out.print("  ");
+			
+		}
+			for(int k=i;k<rows;k++) {
+		System.out.print("* ");
+
+	}
+			System.out.println();
+
+	}
+	}
+}

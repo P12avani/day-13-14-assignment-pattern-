ba48b46534743a77570b77837f3f7b2d0672b7c8
pavani polipalli <polipallipavani54@gmail.com>
2025-10-30 13:29:13 +0530

    NumberTriangle

---
+++ NumberTrianglePattern.java
@@ -0,0 +1,26 @@
+package com.codegnan.patterns;
+
+public class NumberTrianglePattern {
+		    public static void main(String[] args) {
+		        int n = 6; // Number of rows
+
+		        for (int i = 1; i <= n; i++) {
+		            // Print leading spaces
+		            for (int j = i; j < n; j++) {
+		                System.out.print(" ");
+		            }
+
+		            // Print numbers
+		            for (int j = 1; j <= i; j++) {
+		                System.out.print(i + " ");
+		            }
+
+		            System.out.println();
+		        }
+		    }
+		
+
+
+	}
+
+
